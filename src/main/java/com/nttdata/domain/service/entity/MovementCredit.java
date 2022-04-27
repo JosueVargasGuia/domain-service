196a885393bd75f21c1d218fbb0052c42a86b7c9
JosueVargasGuia <44180784+JosueVargasGuia@users.noreply.github.com>
2022-04-27 15:34:36 -0500

    cambio de modelo

--- src/main/java/com/nttdata/domain/service/entity/MovementCredit.java
+++ src/main/java/com/nttdata/domain/service/entity/MovementCredit.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+ 
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,12 +23,12 @@ import lombok.Setter;
 @Document(collection = "movement_credit")
 public class MovementCredit {
 	@Id
-	private Long IdMovementCredit;
+	private Long idMovementCredit;
 	private Long idCreditAccount;
-	private double amount;
+	private Double amount;
 	private Date dateMovement;
 	private TypeMovementCredit typeMovementCredit;
-	double commissionForTransaction;
+	private Double commissionForTransaction;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss") 
 	private Date creationDate;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
